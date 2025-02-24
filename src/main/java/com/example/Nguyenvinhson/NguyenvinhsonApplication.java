88b8a44323ae9e7fffbf34e589a7b7de5e42c675
Nguyen Vinh Son <vinhsonnguyen1998@gmail.com>
2025-02-24 10:46:07 +0700

    Buoi 1

---
+++ src/main/java/com/example/Nguyenvinhson/NguyenvinhsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nguyenvinhson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenvinhsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenvinhsonApplication.class, args);
+	}
+
+}
